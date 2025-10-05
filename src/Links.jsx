6fad73a22ecaf02a00721ec395f32d99ad60e713
myna <myna.vu@students.cis.edu.vn>
2025-10-05 14:28:31 +1300

    updated resume properly

--- src/Links.jsx
+++ src/Links.jsx
@@ -33,7 +33,7 @@ E-mail
         <div className="smallest flex flex-col banner">
         <p>&nbsp;</p>
         <p className="big">&nbsp;&nbsp;⛅</p>
-        <a className="group red" href='./Resume.pdf' target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> Resume</a>
+        <a className="group red" href='./Resume_2025.pdf' target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> Resume</a>
         <a className="group yellow" href="mailto:avu13@uclive.ac.nz">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> E-Mail</a>
         <a className="group green" href="https://github.com/mynavu" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> GitHub</a>
         <a className="group blue" href="https://www.linkedin.com/in/myna-vu-237091322/" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> LinkedIn</a>
