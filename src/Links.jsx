299bce68c343850ab415d0faec84ef35607e6f46
myna <myna.vu@students.cis.edu.vn>
2026-04-13 14:20:36 +1200

    updated resume

--- src/Links.jsx
+++ src/Links.jsx
@@ -1,8 +1,8 @@
 import { useRef, useEffect } from "react";
 
 export const Links = () => {
-    const contentRef = useRef(null);
-/*
+  const contentRef = useRef(null);
+  /*
     useEffect(() => {
         if (contentRef.current) {
           contentRef.current.innerHTML = `
@@ -23,27 +23,58 @@ E-mail
         }
       }, []);
       */
-    
 
-    return (
-      <div className="small banner flex flex-col items-center" ref={contentRef}>
-        <pre>
-            {`╭──────────────────────────────────────────────────────────────────────╮`}
-        </pre>
-        <div className="smallest flex flex-col banner">
+  return (
+    <div className="small banner flex flex-col items-center" ref={contentRef}>
+      <pre>
+        {`╭──────────────────────────────────────────────────────────────────────╮`}
+      </pre>
+      <div className="smallest flex flex-col banner">
         <p>&nbsp;</p>
         <p className="big">&nbsp;&nbsp;⛅</p>
-        <a className="group red" href='./Resume_2025.pdf' target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> Resume</a>
-        <a className="group yellow" href="mailto:avu13@uclive.ac.nz">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> E-Mail</a>
-        <a className="group green" href="https://github.com/mynavu" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> GitHub</a>
-        <a className="group blue" href="https://www.linkedin.com/in/myna-vu-237091322/" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> LinkedIn</a>
-        <a className="group purple" onClick={()=>{alert("please be serious")}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="group-hover:hidden">⬦</span><span className="hidden group-hover:inline">⬥</span> Roblox</a>
+        <a className="group red" href="./Resume_2026.pdf" target="_blank">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="group-hover:hidden">⬦</span>
+          <span className="hidden group-hover:inline">⬥</span> Resume
+        </a>
+        <a className="group yellow" href="mailto:avu13@uclive.ac.nz">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="group-hover:hidden">⬦</span>
+          <span className="hidden group-hover:inline">⬥</span> E-Mail
+        </a>
+        <a
+          className="group green"
+          href="https://github.com/mynavu"
+          target="_blank"
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="group-hover:hidden">⬦</span>
+          <span className="hidden group-hover:inline">⬥</span> GitHub
+        </a>
+        <a
+          className="group blue"
+          href="https://www.linkedin.com/in/myna-vu-237091322/"
+          target="_blank"
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="group-hover:hidden">⬦</span>
+          <span className="hidden group-hover:inline">⬥</span> LinkedIn
+        </a>
+        <a
+          className="group purple"
+          onClick={() => {
+            alert("please be serious");
+          }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="group-hover:hidden">⬦</span>
+          <span className="hidden group-hover:inline">⬥</span> Roblox
+        </a>
         <p>&nbsp;</p>
-        </div>
-        <pre>
-            {`╰──────────────────────────────────────────────────────────────────────╯`}
-        </pre>
       </div>
-    );
-  };
-  
+      <pre>
+        {`╰──────────────────────────────────────────────────────────────────────╯`}
+      </pre>
+    </div>
+  );
+};
